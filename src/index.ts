a2969e0915ed357a4ff8015664e02211a573f524
Andreas Antonsson <antonsson.andreas@hotmail.com>
2023-12-27 21:39:47 +0100

    gitignore

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from "express"
 import { corsMiddleware } from "./middlewares/corsMiddleware"
 import { rateLimitMiddleware } from "./middlewares/limitMiddleware"
-import oldTimeStrong from "./endpoints/oldtimestrong"
+import oldTimeStrong from "./endpoints/otstrong"
 import gripStrength from "./endpoints/gripstrength"
 import stoneStrength from "./endpoints/stonestrength"
 import armWrestling from "./endpoints/armwrestling"
